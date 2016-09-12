91bb0334d5ad86cab195b9355ccc0f7fac4aa41f
Anthony <Home>
2016-09-12 16:59:30 -0500

    Initial commit

---
+++ src/com/anthony/Main.java
@@ -0,0 +1,45 @@
+package com.anthony;
+
+import java.util.Scanner;
+
+public class Main {
+    // Scanners
+    static Scanner stringScanner = new Scanner(System.in);
+    static Scanner numberScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Create variables and constants
+        double DISTANCE_TOTAL = 26.2;
+        double distance = 1.0;
+        int weekNum = 1;
+        double percentage = 0.01;
+        // 1% increase each week
+        weeklySchedule(DISTANCE_TOTAL, distance, weekNum, percentage);
+        // 5% increase each week
+        percentage = 0.05;
+        weeklySchedule(DISTANCE_TOTAL, distance, weekNum, percentage);
+        // 10% increase each week
+        percentage = 0.10;
+        weeklySchedule(DISTANCE_TOTAL, distance, weekNum, percentage);
+
+
+        // Close scanners
+        stringScanner.close();
+        numberScanner.close();
+    }
+
+    private static void weeklySchedule(double DISTANCE_TOTAL, double distance, int weekNum, double percentage) {
+        while (distance <= DISTANCE_TOTAL) {
+
+            distance = distance + distance * percentage;
+            weekNum++;
+//            System.out.print("Weeknumber: " + weekNum);
+//            System.out.println(String.format(" Distance run: %.2f", distance));
+
+        }
+        System.out.println("It will take " + weekNum + " weeks to reach your goal of " + DISTANCE_TOTAL + " miles");
+        System.out.println(String.format("Distance run: %.2f",  distance));
+    }
+
+}
